75b89dabe9fcfb5f348754f65eeb62ec803fd13c
Craig E. Despeaux <cedespeaux@verisign.com>
2014-01-10 14:29:08 -0500

    Added tests for getdns_dict_set_bindata()

--- src/test/check_getdns.c
+++ src/test/check_getdns.c
@@ -25,6 +25,7 @@
 #include "check_getdns_dict_destroy.h"
 #include "check_getdns_dict_set_dict.h"
 #include "check_getdns_dict_set_list.h"
+#include "check_getdns_dict_set_bindata.h"
 
 int
 main (void)
@@ -49,6 +50,7 @@ main (void)
   Suite *getdns_dict_destroy_suite(void);
   Suite *getdns_dict_set_dict_suite(void);
   Suite *getdns_dict_set_list_suite(void);
+  Suite *getdns_dict_set_bindata_suite(void);
 
   sr = srunner_create(getdns_general_suite());
   srunner_add_suite(sr, getdns_general_sync_suite());
@@ -67,6 +69,7 @@ main (void)
   srunner_add_suite(sr, getdns_dict_destroy_suite());
   srunner_add_suite(sr, getdns_dict_set_dict_suite());
   srunner_add_suite(sr, getdns_dict_set_list_suite());
+  srunner_add_suite(sr, getdns_dict_set_bindata_suite());
 
   srunner_set_log(sr, "check_getdns.log");
   srunner_run_all(sr, CK_NORMAL);
