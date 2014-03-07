9c5d8aa6871e227a9dd02e5cebb75debf426afe3
Glen Wiley <gwiley@verisign.com>
2014-03-07 02:50:00 -0500

    fixed detecting libevent v1 if libevent v2 is not available

--- src/example/getdns_libevent.h
+++ src/example/getdns_libevent.h
@@ -27,10 +27,11 @@
 #include <getdns/getdns.h>
 #include <getdns/getdns_ext_libevent.h>
 #ifdef HAVE_EVENT2_EVENT_H
-#include <event2/event.h>
+    #include <event2/event.h>
 #else
-#  ifndef u_char
-#    define u_char unsigned char
-#  endif
-#include <event.h>
+    #include <event.h>
+#endif
+
+#ifndef HAVE_U_CHAR
+    typedef unsigned char u_char;
 #endif
