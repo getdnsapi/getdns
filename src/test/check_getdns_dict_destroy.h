b488432c240a2dfaec2b4e47039cc4b22f4eac75
ngoyal <ngoyal@gmail.com>
2014-03-12 21:20:17 -0400

    Fix for undeclared SIGABRT

--- src/test/check_getdns_dict_destroy.h
+++ src/test/check_getdns_dict_destroy.h
@@ -27,6 +27,8 @@
 #ifndef _check_getdns_dict_destroy_h_
 #define _check_getdns_dict_destroy_h_
 
+#include <sys/signal.h>
+
     /*
      **************************************************************************
      *                                                                        *
