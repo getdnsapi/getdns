3468ea13e61f95732975dbab5c8ae4f1f644ccdb
Willem Toorop <willem@nlnetlabs.nl>
2014-09-09 15:42:28 +0200

    And not actually include on-board ldns2 (gldns)

---
+++ src/gldns/import.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Meant to be run from this directory
+
+svn co http://unbound.net/svn/trunk/ldns/
+for f in ldns/*.[ch]
+do
+	sed -e 's/sldns_/gldns_/g' -e 's/LDNS_/GLDNS_/g' -e 's/include "ldns/include "gldns/g' -e 's/<ldns\/rrdef\.h>/"gldns\/rrdef.h"/g' $f > ${f#ldns/}
+done
+rm -r ldns
