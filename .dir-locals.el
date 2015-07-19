0dc08a30ce9dae822b9371059b8b452c57de4bc4
Daniel Kahn Gillmor <dkg@fifthhorseman.net>
2015-07-19 11:35:13 +0200

    tell emacs users how to follow coding conventions

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;; emacs local configuration settings for getdns source
+;; surmised by dkg on 2015-07-19 10:10:51+0200
+
+((c-mode
+  (indent-tabs-mode . t)
+  (tab-width . 8)
+  (c-basic-offset . 8)
+  (c-file-style . "linux"))
+ )
