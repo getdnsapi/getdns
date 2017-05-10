760b813333cdbf432d3e89e79e572018240809c5
Willem Toorop <willem@nlnetlabs.nl>
2017-05-10 23:01:57 +0200

    Schedule resource depletion tpkg's sequentually

--- src/test/tpkg/run-parallel.sh
+++ src/test/tpkg/run-parallel.sh
@@ -11,29 +11,45 @@ retry:
 
 MAKEFILE_HEADER
 
+# Resource depletion tests should be performed one-by-one after all
+# other tests have been done.
+#
+RD_TESTS=""
+OTHERS=""
 ALL="results:"
-for TEST_PKG in ${SRCDIR}/*.tpkg
+for TEST_PKG in `echo ${SRCDIR}/*.tpkg | xargs -n1 echo | sort`
 do
-	P=${TEST_PKG#${SRCDIR}/}
-	P=${P%.tpkg}
-	DONE="result.$P"
-	ALL="$ALL $DONE"
+	P="${TEST_PKG#${SRCDIR}/}"
+	P="${P%.tpkg}"
+	R="result.${P}"
+	ALL="${ALL} ${R}"
+	if grep -q 'Category:.*Resource depletion' "${TEST_PKG}/${P}.dsc"
+	then
+		RD_TESTS="${R} ${RD_TESTS}"
+	else
+		OTHERS="${OTHERS} ${R}"
+	fi
 done
-echo $ALL >> Makefile
+echo "${ALL}" >> Makefile
 printf '\t"%s" r\n\n' "${TPKG}" >> Makefile
 printf 'clean:\n\t"%s" clean\n\trm -fr build build-stub-only build-event-loops build-static-analysis install scan-build-reports .tpkg.var.master *.info\n\n' "${TPKG}" >> Makefile
-for TEST_PKG in ${SRCDIR}/*.tpkg
+for P in ${OTHERS}
 do
-	P=${TEST_PKG#${SRCDIR}/}
-	P=${P%.tpkg}
-	DONE="result.$P:"
+	P="${P#result.}"
+	TEST_PKG="${SRCDIR}/${P}.tpkg"
+	DEPS="result.${P}:"
 	for D in `grep "^Depends: " "${TEST_PKG}/${P}.dsc" | sed 's/^Depends: //g'`
 	do
-		D=${D%.tpkg}
-		DONE="$DONE result.$D"
+		D="${D%.tpkg}"
+		DEPS="${DEPS} result.${D}"
 	done
-	echo $DONE >> Makefile
+	echo "${DEPS}" >> Makefile
 	printf '\t"%s" %s exe "%s"\n\n' "${TPKG}" "$*" "${TEST_PKG}" >> Makefile
-	echo "" >> Makefile
 done
-make -j 2
+for RD in ${RD_TESTS}
+do
+	RD_TESTS="${RD_TESTS#$RD }"
+	TEST_PKG="${RD#result.}"
+	printf '%s: %s %s\n\t"%s" %s exe "%s/%s.tpkg"\n\n' "${RD}" "${OTHERS}" "${RD_TESTS}" "${TPKG}" "$*" "${SRCDIR}" "${TEST_PKG}" >> Makefile
+done
+#make -j 2
