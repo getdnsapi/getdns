ac17d4ebedb10df4af3c997d38327721c7e8c623
Willem Toorop <willem@nlnetlabs.nl>
2017-12-14 11:53:15 +0100

    We need a specific install location for tests builds ...
    to not load default library

--- src/test/tpkg/clean.sh
+++ src/test/tpkg/clean.sh
@@ -3,5 +3,5 @@
 export SRCDIR=`dirname $0`
 (	cd $SRCDIR
 	./tpkg clean
-	rm -fr build build-stub-only build-event-loops build-static-analysis install scan-build-reports .tpkg.var.master *.info Makefile
+	rm -fr build build-stub-only build-event-loops build-static-analysis install install-stub-only install-event-loops install-static-analysis scan-build-reports .tpkg.var.master *.info Makefile
 )
